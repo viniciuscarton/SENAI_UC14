345b089693b55a4db4ad075210300305056cfaa0
viniciuscarton <viniciuscarton@outlook.com>
2022-11-15 23:31:18 -0300

    encontro remoto 1

---
+++ Chapter/Chapter/Chapter/Contexts/ChapterContext.cs
@@ -0,0 +1,25 @@
+﻿using Chapter.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Chapter.Contexts
+{
+    public class ChapterContext: DbContext
+    {
+        public ChapterContext()
+        {
+        }
+        public ChapterContext(DbContextOptions<ChapterContext> options): base(options)
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) 
+        { 
+            if(!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source = CARRIE\\SQLEXPRESS; Initial Catalog = Chapter; Integrated Security = true; TrustServerCertificate=true");
+            }
+        }
+
+        public DbSet<Livro> Livros { get; set; }
+
+    }
+}
---
+++ Chapter/Chapter/Chapter/Models/Livro.cs
@@ -0,0 +1,10 @@
+﻿namespace Chapter.Models
+{
+    public class Livro
+    {
+        public int Id { get; set; }
+        public string? Titulo { get; set; }
+        public int? Paginas { get; set; }
+        public bool Disponivel { get; set; }
+    }
+}
